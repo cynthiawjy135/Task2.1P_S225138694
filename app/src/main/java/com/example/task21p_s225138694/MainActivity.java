88466deb6d0c4adbac7c203ba74cf319e5d7a7f8
cynthiawjy135 <s225138694@deakin.edu.au>
2026-03-11 11:01:07 +1100

    Created UI Task

--- app/src/main/java/com/example/task21p_s225138694/MainActivity.java
+++ app/src/main/java/com/example/task21p_s225138694/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.task21p_s225138694;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //SOURCE SPINNER
+        Spinner spinnerSource = (Spinner) findViewById(R.id.spinner_source);
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapter_src = ArrayAdapter.createFromResource(
+                this,
+                R.array.source_array,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        adapter_src.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        spinnerSource.setAdapter(adapter_src);
+
+        //DESTINATION SPINNER
+        Spinner spinnerDestination = (Spinner) findViewById(R.id.spinner_destination);
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapter_dest = ArrayAdapter.createFromResource(
+                this,
+                R.array.destination_array,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        adapter_dest.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        spinnerDestination.setAdapter(adapter_dest);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
