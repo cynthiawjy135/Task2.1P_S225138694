50c193fef64ff1b63547a9261d0dc6fd0c6cfccb
cynthiawjy135 <s225138694@deakin.edu.au>
2026-03-16 01:34:33 +1100

    Updated UI constraints and spinner function

--- app/src/main/java/com/example/task21p_s225138694/MainActivity.java
+++ app/src/main/java/com/example/task21p_s225138694/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.task21p_s225138694;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -48,6 +50,62 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        spinnerSource.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selected = parent.getItemAtPosition(position).toString();
+                ArrayAdapter<CharSequence> adapter;
+
+                if (selected.equals("USD")) {
+                    adapter = ArrayAdapter.createFromResource(
+                            MainActivity.this,
+                            R.array.currency_array,
+                            android.R.layout.simple_spinner_item
+                    );
+                } else if (selected.equals("mpg")) {
+                    adapter = ArrayAdapter.createFromResource(
+                            MainActivity.this,
+                            R.array.fuel_kmL_array,
+                            android.R.layout.simple_spinner_item
+                    );
+                } else if (selected.equals("Gallon")) {
+                    adapter = ArrayAdapter.createFromResource(
+                            MainActivity.this,
+                            R.array.fuel_L_array,
+                            android.R.layout.simple_spinner_item
+                    );
+                } else if (selected.equals("Nautical Mile")) {
+                    adapter = ArrayAdapter.createFromResource(
+                            MainActivity.this,
+                            R.array.fuel_km_array,
+                            android.R.layout.simple_spinner_item
+                    );
+                } else if (selected.equals("Celcius")) {
+                    adapter = ArrayAdapter.createFromResource(
+                            MainActivity.this,
+                            R.array.temp_fromCelcius_array,
+                            android.R.layout.simple_spinner_item
+                    );
+                }
+                else {
+                    adapter = ArrayAdapter.createFromResource(
+                            MainActivity.this,
+                            R.array.temp_fromFahrenheit_array,
+                            android.R.layout.simple_spinner_item
+                    );
+                }
+
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerDestination.setAdapter(adapter);
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
         });
     }
 }
